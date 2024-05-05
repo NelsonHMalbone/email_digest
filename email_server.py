d1bfaedbc9340a110a2f16c3c7031eb0cba96b4a
Nelson Malbone <nhmalbone@icloud.com>
2024-05-05 18:52:33 -0400

    day 27 email works

--- email_server.py
+++ email_server.py
@@ -1,5 +1,17 @@
-import os
 import smtplib
 import ssl
 import config
 
+def send_email_script(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = config.username
+    api_password = config.app_password
+
+    reciever = 'malbonenelson@gmail.com'
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, api_password)
+        server.sendmail(username, reciever, message)
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import requests
 import config
+from email_server import send_email_script
 
 # api key
 api_key = config.api_key
-url = f"https://newsapi.org/v2/everything?q=apple&from=2024-05-01&to=2024-05-01&sortBy=popularity&apiKey={api_key}"
+url = f'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api_key}'
+
 
 # make request
 request = requests.get(url)
 
 # getting content using a dictionary with data
 content = request.json()
-
 # accessing article and or descriptions
+body = ''
 for article in content['articles']:
-    print(article['title'])
-    print()
-    print(article['description'])
+    if article['description'] is not None:
+        body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+
+body = body.encode('utf-8')
+send_email_script(message=body)
